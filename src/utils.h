3f73a1059b811cfbbc1e0c0372f21303b442ec43
Francesco <francescopaul789@gmail.com>
2025-03-03 12:32:07 +0100

    trivial: assert message

--- src/utils.h
+++ src/utils.h
@@ -17,18 +17,19 @@
   if (!(cond)){                                           \
     fprintf(stdout, "\033[2J\033[H\n");                   \
     tcsetattr(STDIN_FILENO, TCSANOW, &term_orig);         \
-    fprintf(stderr, "%s, %d\n", __FILE__, __LINE__);                  \
-    fprintf(stderr, "ASSERTION (" #cond ") FAILED:\n");   \
+    fprintf(stderr, "ASSERTION FAILED:" #cond "\n");   \
+    fprintf(stderr, "%s, %d\n", __FILE__, __LINE__);      \
     exit(1);                                              \
   }                                                       \
 } while(0)
 
-#define ASSERT_MSG(cond, format, ...) do {                        \
+#define ASSERT_MSG(cond, format, ...) do {                \
   if (!(cond)){                                           \
     fprintf(stdout, "\033[2J\033[H\n");                   \
     tcsetattr(STDIN_FILENO, TCSANOW, &term_orig);         \
-    fprintf(stderr, "ASSERTION (" #cond ") FAILED:\n");   \
-    fprintf(stderr, format, __VA_ARGS__);                         \
+    fprintf(stderr, "ASSERTION FAILED: " #cond "\n");   \
+    fprintf(stderr, "%s, %d\n", __FILE__, __LINE__);      \
+    fprintf(stderr, format, __VA_ARGS__);                 \
     fprintf(stderr, "\n");                                \
     exit(1);                                              \
   }                                                       \
